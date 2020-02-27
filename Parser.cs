a112a35c65cdba9c664b37e9e1574182260039ae
Wentian Windows PC <wbai1295@gmail.com>
2020-02-27 15:38:29 -0500

    has a few bug

--- Parser.cs
+++ Parser.cs
@@ -6,39 +6,57 @@ using System.Threading.Tasks;
 using System.Globalization;
 using System.IO;
 using System.Text.RegularExpressions;
+using OfficeOpenXml;
 
 namespace GradeClassifier
 {
     class Parser
     {
-        String fileName;
         CompareInfo Compare; // ignore upper or lower case
+        int idIndex;
+        String fileName;
+        Dictionary<String, List<String>> classifier;
+        List<String[]> others;
 
         public Parser() {
             Compare = CultureInfo.InvariantCulture.CompareInfo;
+            idIndex = 3;
+            classifier = new Dictionary<string, List<string>>();
+            others = new List<string[]>();
         }
 
         public Parser(string fileName) {
             this.fileName = fileName;
             Compare = CultureInfo.InvariantCulture.CompareInfo;
+            classifier = new Dictionary<string, List<string>>();
+            others = new List<string[]>();
         }
 
+        // Initialize all global variables
+        public void initialization() {
+            classifier.Clear();
+            idIndex = 3;
+            fileName = "";
+        }
+
+        // Read given file (default is CSV file) and parse it.
         public void ReadData(string fileName) {
+            initialization();
+
             try {
-                this.fileName = fileName;
-                // Create an instance of StreamReader to read from a file.
-                // The using statement also closes the StreamReader.
                 using (StreamReader sr = new StreamReader(fileName)) {
                     string line;
-                    // Read and display lines from the file until the end of 
-                    // the file is reached.
+                    char delimiter = ',';
+                    string[] cells;
                     for (int i = 0; (line = sr.ReadLine()) != null; i++) {
-                        string[] items = splitLine(line);
                         if (i == 0) {
-                            parseHeader(items);
+                            delimiter = findDelimiter(line);
+                            cells = splitLine(line, delimiter);
+                            parseHeader(cells);
                         }
                         else {
-
+                            cells = splitLine(line, delimiter);
+                            parseStudent(cells);
                         }
                     }
                 }
@@ -48,145 +66,90 @@ namespace GradeClassifier
                 Console.WriteLine("File could not be read:");
                 Console.WriteLine(e.Message);
             }
-        }
 
-        // split line into items
-        private string[] splitLine(String line) {
-            string[] items;
-            if (fileName.IndexOf("csv") != -1){
-                items = csvSplitLine(line);
-            }
-            else {
-                items = line.Split('\t');
-            }
-            return items;
-        }
-
-        // split line in CSV file
-        private string[] csvSplitLine(String line) {
-            string[] items = line.Split(',');
-            List<String> merge = new List<string>();
-            for (int i = 0; i < items.Length; i++) {
-                string item = items[i];
-                if (item.IndexOf('\"') != -1) {
-                    item += items[i + 1];
-                    i++;
-                }
-                merge.Add(item);
-            }
-            return merge.ToArray<string>();
-        }
-
-
-        //------------------------------------------------------------------------
-        //----------------------------- Header Parse -----------------------------
-        //------------------------------------------------------------------------
-
-        // parse first row, iterate all headers and create Property classes for them
-        private void parseHeader(string[] headers) {
-            for (int i = 4; i < headers.Length; i++) {
-                string header = headers[i];
-                string type = headerType(header);
-                
-            }
-        }
-
-        // find out which type header is
-        // return Weight, Total, Attendance, Lab, Assignment, Quiz, Exam, or None
-        private String headerType(string header) {
-            int bracket = header.IndexOf('[');
-            if (bracket == -1)
-                return "None";
-
-            String colType;
-            int pos;
-            header = header.Substring(0, bracket);
-            if ((pos = Compare.IndexOf(header, "Weight", CompareOptions.IgnoreCase)) != -1) {
-                colType = "Weight";
-            }
-            else if ((pos = Compare.IndexOf(header, "Total", CompareOptions.IgnoreCase)) != -1) {
-                colType = "Total";
-            }
-            else if ((pos = Compare.IndexOf(header, "attend", CompareOptions.IgnoreCase)) != -1) {
-                colType = "Attendance";
-            }
-            else if ((pos = Compare.IndexOf(header, "lab", CompareOptions.IgnoreCase)) != -1) {
-                colType = "Lab";
-            }
-            else if ((pos = Compare.IndexOf(header, "homework", CompareOptions.IgnoreCase)) != -1) {
-                colType = "Assignment";
-            }
-            else if ((pos = Compare.IndexOf(header, "assign", CompareOptions.IgnoreCase)) != -1) {
-                colType = "Assignment";
-            }
-            else if ((pos = Compare.IndexOf(header, "quiz", CompareOptions.IgnoreCase)) != -1) {
-                colType = "Quiz";
-            }
-            else if ((pos = Compare.IndexOf(header, "exam", CompareOptions.IgnoreCase)) != -1) {
-                colType = header;
-            }
-            else {
-                colType = "None";
-            }
-
-            return colType;
-        }
-
-        private String getName(string header) {
-            return header.Substring(0, header.IndexOf('['));
-        }
-
-        // find out which type point is
-        // return Score, Complete/Incomplete, Letter, or ?
-        private String pointsType(string header) {
-            int bracket1 = header.IndexOf('[');
-            int bracket2 = header.LastIndexOf(']');
-            String grade = header.Substring(bracket1, bracket2 - bracket1);
-            String pointsType;
-            if (Compare.IndexOf(grade, "score", CompareOptions.IgnoreCase) != -1)
-            {
-                pointsType = "Score";
-            }
-            else if (Compare.IndexOf(grade, "Complete", CompareOptions.IgnoreCase) != -1)
-            {
-                pointsType = "Complete/Incomplete";
-            }
-            else if (Compare.IndexOf(grade, "letter", CompareOptions.IgnoreCase) != -1)
-            {
-                pointsType = "Letter";
-            }
-            else
-            {
-                pointsType = "?";
-            }
-
-            return pointsType;
-        }
-
-        // get maximum range of score
-        private int pointsMax(string header) {
-
-            int bracket1 = header.IndexOf('[');
-            int bracket2 = header.LastIndexOf(']');
-            string subHeader = header.Substring(bracket1, bracket2 - bracket1);
-            string pointStr = new String(subHeader.Where(Char.IsDigit).ToArray());
-            int point = Int32.Parse(pointStr);
-            return point;
-        }
-
-
-        //------------------------------------------------------------------------
-        //---------------------------- Student Parse -----------------------------
-        //------------------------------------------------------------------------
-
-        // parse all rows except first one, create Student class for them
-        private void parseStudent(string[] items) { 
             
         }
 
+        // find first char which potentially works as delimiter.
+        private char findDelimiter(String header) {
+            String delimiter = ",\t;|^";
+            foreach (char c in header.ToCharArray()) {
+                if (delimiter.IndexOf(c) != -1)
+                    return c;
+            }
+            return ',';
+        }
+
+        // split line into items
+        private string[] splitLine(String line, char delimiter) {
+            string[] cells = line.Split(delimiter);
+            return cells;
+        }
+
+        // parse first row, iterate each cells and find which column is student id
+        private void parseHeader(string[] headers) {
+            for (int i = 0; i < headers.Length; i++) {
+                string header = headers[i];
+                int pos;
+                if ((pos = Compare.IndexOf(header, "Student ID", CompareOptions.IgnoreCase)) != -1) {
+                    idIndex = pos;
+                }
+            }
+        }
+
+
+        // parse rows and get student id and grade
+        private void parseStudent(string[] cells) {
+            String studentId = trimQuotes(cells[idIndex]);
+            String grade = trimQuotes(cells[cells.Length-1]);
+            grade = grade.ToUpper();
+            if (isGradeLetter(grade)) {
+                if (!classifier.ContainsKey(grade)) {
+                    classifier[grade] = new List<string>();
+                }
+                classifier[grade].Add(studentId);
+            } else {
+                grade = trimQuotes(cells[cells.Length - 1]);
+                if (string.IsNullOrEmpty(grade) || string.IsNullOrWhiteSpace(grade))
+                    grade = "no grade";
+                others.Add(new string[] { studentId, grade });
+            }
+        }
+
+        // Check if final grade is a letter, e.g. A B C D F 
+        private Boolean isGradeLetter(String grade) {
+            if (string.IsNullOrEmpty(grade) || string.IsNullOrWhiteSpace(grade))
+                return false;
+            char[] letters = { 'A', 'B', 'C', 'D', 'F' };
+            if (!letters.Contains(grade[0]))
+                return false;
+            return true;
+        }
+
+        // remove quotes (") from cell
+        private String trimQuotes(String cell) {
+            return cell.Replace("\"", "");
+        }
+
+        // get classifier
+        public Dictionary<String, List<String>> getClassifier() {
+            return classifier;
+        }
 
         public void print() {
+            foreach(String key in classifier.Keys) {
+                Console.Write(key + ": ");
+                foreach (string id in classifier[key])
+                    Console.Write(id+" ");
+                Console.WriteLine();
+            }
 
+            if (others.Count > 0) {
+                Console.WriteLine("Others:");
+                foreach (String[] other in others) {
+                    Console.WriteLine(other[0] + ": " + other[1]);
+                }
+            }
         }
     }
 }
