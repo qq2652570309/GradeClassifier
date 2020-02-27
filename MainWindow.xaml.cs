f83dcb020951a50ce5f46a0fa328eb890654f0fc
Wentan <wentan1995@gmail.com>
2020-02-27 15:37:11 -0500

    before merge

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -145,42 +145,79 @@ namespace GradeClassifier {
             }
         }
 
-        private void GenerateCSV(String oldFile) {
-            int pos = oldFile.LastIndexOf("\\");
-            String newFile = oldFile.Substring(0, pos + 1) + "out.csv";
-            File.AppendAllText(newFile, "Last Name,First Name,UserName,Student ID\n");
-        }
-
-        /*
-        check whether input scales are valid. If there exit an overlapping, scale is invalid 
-        */
-        private bool isValidScale(int ceiling, int floor, int preFloor, string grade) {
-            if (ceiling <= 0 || floor <= 0 || preFloor <= 0) {
-                MessageBox.Show("Scale must be positive and non-zero");
-                return false;
-            }
-            if (preFloor <= ceiling || ceiling <= floor) {
-                MessageBox.Show("There is overlapping with scale " + grade);
-                return false;
-            }
-            return true;
-        }
-
         // Browse button event
         private void btnOpenFile_Click(object sender, RoutedEventArgs e) {
             string fileName = GetFileName();
             parser.ReadData(fileName);
             parser.print();
 
-            
+            CurrPath.Text = fileName;
         }
 
         private void renameFile_Click(object sender, RoutedEventArgs e) {
 
         }
 
+        //TextBox TcTb; // TermCodeTBox
+        //ComboBox ScTb; // SubjectCodeTBox
+        //TextBox CnTb; // CatalogNumberTBox
+        //TextBox StpTb; // StPCodeTBox
+        //ComboBox gcb; // GradeCBox
+
+        private void GenerateCSV(String oldFile, string fileName) {
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+            List<string> aList = new List<string>() { "946630449", "338835543", "903951927" };
+            dict.Add("A", aList);
+            List<string> aMinusList = new List<string>() { "946630449", "338835543", "903951927" };
+            dict.Add("A-", aMinusList);
+            List<string> bPlusList = new List<string>() { "946630449", "338835543", "903951927" };
+            dict.Add("B+", bPlusList);
+            List<string> bList = new List<string>() { "946630449", "338835543", "903951927" };
+            dict.Add("B", bList);
+            List<string> bMinusList = new List<string>() { "946630449", "338835543", "903951927" };
+            dict.Add("B-", bMinusList);
+            List<string> cList = new List<string>() { "946630449", "338835543", "903951927" };
+            dict.Add("C", cList);
+            List<string[]> others = new List<string[]>();
+
+            int pos = oldFile.LastIndexOf("\\");
+            //String newFile = oldFile.Substring(0, pos + 1) + "out.csv";
+            fileName = oldFile.Substring(0, pos + 1) + fileName;
+            string grade = gcb.Text;
+            string spc = StpTb.Text;
+
+            foreach (KeyValuePair<string, List<string>> entry in dict) {
+                string newName = fileName + "_" + entry.Key + ".csv";
+                List<string> ids = entry.Value;
+
+                File.AppendAllText(newName, "SU,GRAD,Student¡¯s program code,Student¡¯s SUID number,Final Exam Grade\n");
+                foreach (string id in ids) {
+                    string insert = string.Join(",", new string[] { "SU", grade, spc, id, entry.Key}) + "\n";
+                    File.AppendAllText(newName, insert);
+                }
+                Console.WriteLine("grade {0} finish!", entry.Key);
+            }
+
+            if (others.Count > 0) {
+                string newName = fileName + "_others.csv";
+                File.AppendAllText(newName, "SU,GRAD,Student¡¯s program code,Student¡¯s SUID number,Final Exam Grade\n");
+                foreach (string[] strs in others) {
+                    string insert = string.Join(",", new string[] { "SU", grade, spc, strs[0], strs[1] }) + "\n";
+                    File.AppendAllText(newName, insert);
+                }
+                MessageBox.Show("There are problems with the data of " + others.Count + " students. Students' data is stored in "+ newName);
+                Console.WriteLine("others finish!");
+            }
+
+        }
+
         // verify 3 textbox, guarantee none is empty. Gnerate output CSV
         private void PublishClick(object sender, RoutedEventArgs e) {
+            if (string.IsNullOrWhiteSpace(CurrPath.Text)) {
+                MessageBox.Show("Please choose a file");
+                return;
+            }
+
             if (string.IsNullOrWhiteSpace(TcTb.Text)) {
                 MessageBox.Show("Term Code can not be blank");
                 return;
@@ -200,6 +237,8 @@ namespace GradeClassifier {
 
             string fileName = TcTb.Text + "_" + ScTb.Text + "_" + CnTb.Text;
             Console.WriteLine(fileName);
+
+            GenerateCSV(CurrPath.Text, fileName);
         }
 
     }
